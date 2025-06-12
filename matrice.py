fcb663c5ebbdb58ffe836c4403a1c4b9119e8eca
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2025-06-12 14:04:30 +0200

    Update matrice.py

--- matrice.py
+++ matrice.py
@@ -11,19 +11,6 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 # Désactiver l'option de réduction de la largeur de l'application
 st.set_page_config(layout="centered")
 
-# CSS personnalisé pour forcer l'application à utiliser toute la largeur disponible
-st.markdown(
-    """
-    <style>
-    .main {
-        max-width: 100%;
-        padding: 0rem 1rem;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Titre et description de l'application
 st.title("Matrice en radar")
 st.write("Représenter un tableau à deux entrées en forme de radar, et ce quelque soit le type de données évaluées")
@@ -53,8 +40,8 @@ if choice_ticker == 'Tous':
 # Ajouter une option pour choisir si le texte doit être raccourci (wrap)
 wrap_text = st.checkbox("Raccourcir le texte", value=True)
 
-# Ajouter un curseur pour ajuster la taille de la police des légendes
-legend_font_size = st.slider("Taille de la police des légendes", min_value=6, max_value=20, value=10)
+# Ajouter un curseur pour ajuster la taille de la police des étiquettes des axes
+label_font_size = st.slider("Taille de la police des étiquettes des axes", min_value=6, max_value=20, value=10)
 
 # Round the values
 df = df.round(1)
@@ -67,13 +54,13 @@ cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bins)
 
 # Style the DataFrame
 styled_df = df.style.set_properties(
-    **{'text-align': 'center', 'white-space': 'normal'}  # Enable text wrapping
+    **{'text-align': 'center', 'white-space': 'normal'}
 ).set_table_styles([
     {'selector': 'th', 'props': [('font-weight', 'bold'), ('text-align', 'center'), ('white-space', 'normal')]},
     {'selector': 'td:first-child', 'props': [('text-align', 'left')]},
     {'selector': 'td, th', 'props': [('border', '1px solid lightgray')]},
-    {'selector': 'table', 'props': [('border', 'none'), ('table-layout', 'fixed'), ('width', '100%')]},  # Fixed table layout
-    {'selector': 'th, td', 'props': [('width', f'{100 / len(df.columns)}%')]},  # Adjust the width of all columns to be equal
+    {'selector': 'table', 'props': [('border', 'none'), ('table-layout', 'fixed'), ('width', '100%')]},
+    {'selector': 'th, td', 'props': [('width', f'{100 / len(df.columns)}%')]},
 ]).format(precision=1).background_gradient(cmap=cm, axis=None)
 
 # The attributes we want to use in our radar plot.
@@ -97,7 +84,7 @@ fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
 # List of colors for the border
 border_colors = ['#FF8700', '#4682B4', '#32CD32', '#FFD700', '#8A2BE2']
 # List of line styles
-line_styles = ['-', '--', ':', (0, (5, 1, 1, 1, 1, 1))]  # Adding double line style
+line_styles = ['-', '--', ':', (0, (5, 1, 1, 1, 1, 1))]
 # List of line widths
 line_widths = [1, 1.5]
 # Constant fill color
@@ -139,7 +126,7 @@ ax.set_theta_offset(np.pi / 2)
 ax.set_theta_direction(-1)
 
 # Draw axis lines for each angle and label.
-ax.set_thetagrids(np.degrees(angles), labels)
+ax.set_thetagrids(np.degrees(angles), labels, fontsize=label_font_size)
 
 # Go through labels and adjust alignment based on where it is in the circle.
 for label, angle in zip(ax.get_xticklabels(), angles):
@@ -157,19 +144,14 @@ ax.set_ylim(0, 5)
 ax.set_rlabel_position(180 / num_vars)
 
 # Add some custom styling.
-# Change the color of the tick labels.
 ax.tick_params(colors='#222222')
-# Make the y-axis (0-100) labels smaller.
 ax.tick_params(axis='y', labelsize=8)
-# Change the color of the circular gridlines.
 ax.grid(color='#AAAAAA')
-# Change the color of the outermost gridline (the spine).
 ax.spines['polar'].set_color('#222222')
-# Change the background color inside the circle itself.
 ax.set_facecolor('#FAFAFA')
 
 # Add a legend as well.
-ax.legend(loc='upper right', bbox_to_anchor=(1.9, 1.1), prop={'size': legend_font_size})
+ax.legend(loc='upper right', bbox_to_anchor=(1.9, 1.1))
 
 # Display the plot in Streamlit
 st.pyplot(fig)
